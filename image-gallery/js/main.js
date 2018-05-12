1deb31a07492f1d698b51e39f7112d7e5f9f7fdc
djordjijem <djordjijem@gmail.com>
2018-05-13 01:46:03 +0200

    add changes

---
+++ image-gallery/js/main.js
@@ -0,0 +1,47 @@
+(function(){
+   "use strict";
+
+   let img = document.querySelectorAll('.img-gallery');
+   let container = document.getElementById('container');
+   let modal = document.querySelector('.modal');
+  
+
+   function makeImageResponsive(image,div){
+       image.style.width = "100%";
+       image.style.heigth = 'auto';
+       div.style.position = 'absolute';
+       div.style.left = '20%';
+       div.style.top = '10%';
+       div.style.width = '60%'; 
+       div.style.animation = 'zoomInImage 0.6s';
+   }
+
+   function getImage(event){
+   if(document.querySelector('.new_div')) {
+       let old_div = document.querySelector('.new_div');
+       old_div.parentElement.removeChild(old_div);
+   } 
+    let new_div = document.createElement('div');
+     new_div.classList.add('new_div');
+     modal.appendChild(new_div);
+     modal.style.display = 'block'; 
+     let clonedImg = this.cloneNode(true);
+     makeImageResponsive(clonedImg, new_div);
+     new_div.appendChild(clonedImg);
+     window.addEventListener('click', function (event) {
+         if(event.target === modal){
+             modal.style.display = 'none';
+         }
+         
+       });
+     
+   }
+
+   img.forEach(function(item){
+    item.addEventListener('click', getImage);
+   });
+   
+
+
+
+})();
