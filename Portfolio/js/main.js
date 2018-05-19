a0378b38ae51564a5f94f14687ed234aac0ac82a
djordjijem <djordjijem@gmail.com>
2018-05-19 13:45:28 +0200

    add changes

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -42,10 +42,92 @@ let section = document.querySelectorAll('section');
 (function(){
     "use strict";
 
+var projects_content = document.querySelector('.projects-holder-content');    
 var tabs = document.querySelectorAll('.projects-holder li');
-console.log(tabs);
+var newDiv = document.createElement('div');
+newDiv.classList.add('.hover-effect');
 
+function removeBgColor(){
+    tabs.forEach(function(el){
+        el.style.backgroundColor = 'inherit';
+        el.style.color = 'black';
+    });
+}
+function setColor(el){
+    el.style.backgroundColor = 'rgb(76, 74, 74,0.9)';
+    el.style.color = '#D961AD';
+}
 
+function removeContent(){
+    projects_content.querySelectorAll('.holders').forEach(function(el) {
+        el.style.display = 'none';
+    });
+}
+function setDisplayAndColor(holder){
+    projects_content.querySelector(holder).style.display = 'block';
+    projects_content.querySelector(holder).style.backgroundColor = 'rgb(76, 74, 74,0.9)';
+}
 
+tabs.forEach(function(el){
+    el.addEventListener('click', function(event){
+        
+      switch(this.innerText){
+          case 'Real Calculator':
+            removeContent();
+            setDisplayAndColor('calculator-holder');
+            removeBgColor();
+            setColor(this);
+
+           
+          break;
+          case 'Non-JS UI Components':
+            removeContent();
+            projects_content.querySelector('.non-JS-Components').style.display = 'block';
+            projects_content.querySelector
+            ('.non-JS-Components').style.backgroundColor = 'rgb(76, 74, 74,0.9)';
+            removeBgColor();
+            setColor(this);
+          break;
+          case 'Other Projects':
+            removeContent();
+            projects_content.querySelector('.other-projects').style.display = 'block';
+            projects_content.querySelector('.other-projects').style.backgroundColor = 'rgb(76, 74, 74,0.9)';
+            removeBgColor();
+            setColor(this);
+          break;
+          case 'Forms':
+            removeContent();
+            projects_content.querySelector('.forms').style.display = 'block';
+            projects_content.querySelector('.forms').style.backgroundColor = 'rgb(76, 74, 74,0.9)';
+            removeBgColor();
+            setColor(this);
+          break;
+          case 'Web Component':
+            removeContent();
+            projects_content.querySelector('.web-component').style.display = 'block';
+            projects_content.querySelector('.web-component').style.backgroundColor = 'rgb(76, 74, 74,0.9)';
+            removeBgColor();
+            setColor(this);
+          break;
+          case 'Shopping Cart':
+            removeContent();
+            projects_content.querySelector('.shopping-cart').style.display = 'block';
+            projects_content.querySelector('.shopping-cart').style.backgroundColor = 'rgb(76, 74, 74,0.9)';
+            removeBgColor();
+            setColor(this);
+          break;
+          case 'Clock':
+            removeContent();
+            projects_content.querySelector('.clock').style.display = 'block';
+            projects_content.querySelector('.clock').style.backgroundColor = 'rgb(76, 74, 74,0.9)';
+            removeBgColor();
+            setColor(this);
+          break;
+          default:
+          return false;
+          break;
+      }
+    },false);
+});
 
 })();
